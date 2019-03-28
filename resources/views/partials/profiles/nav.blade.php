2599884e159d21fbb72a521d0f13c41e9dbd52eb
Uzzair Baharudin <uzzaircode@gmail.com>
2019-03-28 17:25:10 +0800

    change profile nav colour to yellowish

--- resources/views/partials/profiles/nav.blade.php
+++ resources/views/partials/profiles/nav.blade.php
@@ -1,10 +1,10 @@
 <ul class="nav nav-pills nav-justified" id="myTab" role="tablist">
     <li class="nav-item">
-        <a class="nav-link active" id="home-tab" data-toggle="tab" href="#home" role="tab" aria-controls="home"
+        <a class="nav-link active font-weight-bold" id="home-tab" data-toggle="tab" href="#home" role="tab" aria-controls="home"
             aria-selected="true">Profile</a>
     </li>
     <li class="nav-item">
-        <a class="nav-link" id="profile-tab" data-toggle="tab" href="#profile" role="tab" aria-controls="profile"
+        <a class="nav-link font-weight-bold" id="profile-tab" data-toggle="tab" href="#profile" role="tab" aria-controls="profile"
             aria-selected="false">My Purchase</a>
     </li>    
 </ul>
